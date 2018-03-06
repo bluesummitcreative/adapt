59811aafdd4f5b4b06d2723cb50526ff683a825a
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-06 14:55:22 -0700

    Update archive-meetings.php

--- archive-meetings.php
+++ archive-meetings.php
@@ -5,33 +5,39 @@
 <div class="container-fluid container-padding">
 	<div class="container">
 		<div class="row">
-		<?php
+			<?php
 /**/
 $years = $wpdb->get_col("SELECT DISTINCT YEAR(post_date)
 FROM $wpdb->posts WHERE post_status = 'publish'
 AND post_type = 'post' ORDER BY post_date DESC");
 foreach($years as $year) :
 ?>
-<li><a href="JavaScript:void()"><?php echo $year; ?></a>
-
-    <ul class="archive-sub-menu">
-        <?    $months = $wpdb->get_col("SELECT DISTINCT MONTH(post_date)
+ 
+ <div id="accordion">
+  <?    $months = $wpdb->get_col("SELECT DISTINCT MONTH(post_date)
         FROM $wpdb->posts WHERE post_status = 'publish' AND post_type = 'post'
         AND YEAR(post_date) = '".$year."' ORDER BY post_date DESC");
         foreach($months as $month) :
         ?>
-            <li><a href="<?php echo get_month_link($year, $month); ?>">
+   <div class="card">
+    <div class="card-header">
+      <h5>
+        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          <?php echo $year; ?>
+        </button>
+      </h5>
+    </div>
 
-                <?php echo date( 'F', mktime(0, 0, 0, $month) );?></a>
-
-            </li>
-
-        <?php endforeach;?>
-
-    </ul>
-
-</li>
-
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body">
+       <a href="<?php echo get_month_link($year, $month); ?>">
+       <?php echo date( 'F', mktime(0, 0, 0, $month) );?></a>
+       
+      </div>
+    </div>
+  </div>
+  <?php endforeach;?>
+</div>
 <?php endforeach; ?>
 			<?php get_sidebar(); ?>
 		</div>
