353835f5762ca8c5f258eb2dc6ffff1dda7fd680
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-06 15:08:53 -0700

    Update archive-meetings.php

--- archive-meetings.php
+++ archive-meetings.php
@@ -5,46 +5,28 @@
 <div class="container-fluid container-padding">
 	<div class="container">
 		<div class="row">
-			<?php
-global $wpdb;
+			<div id="accordion">
+			  <div class="card">
+			    <div class="card-header" id="headingOne">
+			      <h5 class="mb-0">
+				<button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-controls="collapseOne">
+				  Month
+				</button>
+			      </h5>
+			    </div>
 
-$limit = 0;
-$year_prev = null;
-$months = $wpdb->get_results("SELECT DISTINCT MONTH( post_date ) AS month ,	YEAR( post_date ) AS year, COUNT( id ) as post_count FROM $wpdb->posts WHERE post_status = 'publish' and post_date <= now( ) and post_type = 'post' GROUP BY month , year ORDER BY post_date DESC");
-
-foreach($months as $month) :
-
-	$year_current = $month->year;
-	
-	if ($year_current != $year_prev)
-	{
-		if($year_current != date('Y'))
-		{
-		?>
-			</ul>
-		<?php
-		}
-		?>
-
-		<h3>
-			<a href="<?php bloginfo('url') ?>/<?php echo $month->year; ?>/"><?php echo $month->year; ?></a>
-		</h3>	
-		<ul class='list-group'>			
-	<?php 
-	} 
-	?>
-	
-	<li class='list-group-item'>
-		<a href="<?php bloginfo('url') ?>/<?php echo $month->year; ?>/<?php echo date("m", mktime(0, 0, 0, $month->month, 1, $month->year)) ?>"><span class="archive-month"><?php echo date_i18n("F", mktime(0, 0, 0, $month->month, 1, $month->year)) ?></span></a>
-		&nbsp;<span class='label label-default'><?php echo $month->post_count; ?></span>
-	</li>
-	
-	<?php 
-	$year_prev = $year_current;
-
-endforeach; 
-?>
-</ul>
+			    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+			      <div class="card-body">
+				<ul>
+						<li><?php the_title(); ?></li>
+						<li><?php the_title(); ?></li>
+						<li><?php the_title(); ?></li>
+						<li><?php the_title(); ?></li>
+					</ul>
+			      </div>
+			    </div>
+			  </div>
+			</div>
 			<?php get_sidebar(); ?>
 		</div>
 	</div>
