282d583ebc72a2d0497737e2a63f4fd69bbb7401
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-06 15:40:17 -0700

    Update archive-meetings.php

--- archive-meetings.php
+++ archive-meetings.php
@@ -25,19 +25,14 @@
 				<div class="card-header" id="headingOne">
 				  <h5 class="mb-0">
 					<button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-controls="collapseOne">
-					  YEAR
+					  <?php the_title(); ?>
 					</button>
 				  </h5>
 				</div>
 
 				<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
 				  <div class="card-body">
-					<ul>
-						<li><?php the_content(); ?></li>
-						<li><?php the_title(); ?></li>
-						<li><?php the_title(); ?></li>
-						<li><?php the_title(); ?></li>
-					</ul>
+					<?php the_content(); ?>
 				  </div>
 				</div>
 			  </div>
