62d906759d29971aa60c3a1589397da408480984
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-09 12:28:44 -0700

    Update enqueue-styles-scripts.php

--- functions/enqueue-styles-scripts.php
+++ functions/enqueue-styles-scripts.php
@@ -1,7 +1,7 @@
 <?php
 	//Enqueue Styles and Scripts
 	function bsc_enqueue_styles_scripts() {
-		wp_enqueue_style( 'bootstrap.min', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'); // Bootstrap CSS
+		wp_enqueue_style( 'bootstrap.min', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css'); // Bootstrap CSS
 		wp_enqueue_style('font-awesome', '//use.fontawesome.com/releases/v5.0.1/css/all.css'); // Font Awesome
 		wp_enqueue_script( 'html5shiv.min', '//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js' ); //HTML5 Shiv
 		wp_script_add_data( 'html5shiv.min', 'conditional', 'lt IE 9' ); //Get HTML Shiv if less than IE9
