3937b74a535caedbf0d40e7621c887c3a50344a6
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2019-02-18 16:41:07 -0700

    Update two-column.php

--- two-column.php
+++ two-column.php
@@ -33,7 +33,7 @@
 				<div class="news-item" style="background:#c1d1e5;padding:20px 20px 10px 20px;margin-bottom:20px;">
 					<h4><a target="_blank" href="<?php the_field('link'); ?>"><?php the_title(); ?></a></h4>
 				<p><?php the_excerpt(); ?></p>
-				<p><a target="_blank" href="<?php the_field('link'); ?>" class="btn btn-primary">Read More</a></p>
+				<p><a style="margin-left:0;" target="_blank" href="<?php the_field('link'); ?>" class="btn btn-primary">Read More</a></p>
 				</div>
 				<?php }
 			} else {
