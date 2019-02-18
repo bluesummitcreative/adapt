438c75f5c0f7df41bda59545cbb37359cf9cae7c
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2019-02-18 16:57:58 -0700

    Update adapt-publications.php

--- adapt-publications.php
+++ adapt-publications.php
@@ -22,9 +22,9 @@
 			if ( $query->have_posts() ) {
 				while ( $query->have_posts() ) {
 					$query->the_post(); ?>
-				<div class="news-item" style="background:#c1d1e5;padding:20px 20px 10px 20px;margin-bottom:20px;">
+				<div class="news-item">
 					<h4><a target="_blank" href="<?php the_field('link'); ?>"><?php the_title(); ?></a></h4>
-				<p><?php the_excerpt(); ?></p>
+				<p><?php the_content(); ?></p>
 				<p><a style="margin-left:0;" target="_blank" href="<?php the_field('link'); ?>" class="btn btn-primary">Read More</a></p>
 				</div>
 				<?php }
