8e748640ab25874943019d65f49688ad5412da2b
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2019-02-18 16:39:51 -0700

    Update two-column.php

--- two-column.php
+++ two-column.php
@@ -31,8 +31,9 @@
 				while ( $query->have_posts() ) {
 					$query->the_post(); ?>
 				<div class="news-item" style="background:#c1d1e5;padding:20px 20px 10px 20px;margin-bottom:20px;">
-					<h4><a href="<?php the_field('link'); ?>"><?php the_title(); ?></a></h4>
+					<h4><a target="_blank" href="<?php the_field('link'); ?>"><?php the_title(); ?></a></h4>
 				<p><?php the_excerpt(); ?></p>
+				<p><a target="_blank" href="<?php the_field('link'); ?>" class="btn btn-primary">Read More</a></p>
 				</div>
 				<?php }
 			} else {
