9beea46f3ec6d72e037e83ba2725f82d4528833f
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2019-02-19 08:25:36 -0700

    Update header.php

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
 				<div class="row">
 					<div class="col-sm-12">
 						<ul class="list-inline contact-info">
-							<li class="list-inline-item"><a href="#"><i class="fa fa-mobile"></i> (303) 273.3091</a></li>
+							<li class="list-inline-item"><a href="#"><i class="fa fa-mobile"></i> (303) 273-3091</a></li>
 							<li class="list-inline-item"><a href="mailto:ADAPT@mines.edu"><i class="fa fa-envelope"></i> ADAPT@mines.edu</a></li>
 						</ul>
 					</div>
