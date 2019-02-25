d4a05d29b7b9e1f9b558a7bd4cfe408bcb9900ba
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2019-02-25 16:46:40 -0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -21,28 +21,6 @@
 							<a href="https://www.linkedin.com/company/adapt---alliance-for-the-development-of-additive-processing-technologies/?trk=biz-companies-cym" target="_blank"><i class="fa fa-linkedin-square"></i> ADAPTColorado</a>
 							<a href="https://www.instagram.com/adaptcolorado/" target="_blank"><i class="fab fa-instagram"></i> adaptcolorado</a>
 						</div>
-						<div class="row">
-							<!-- Begin Mailchimp Signup Form -->
-<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
-<style type="text/css">
-	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
-	/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-</style>
-<div id="mc_embed_signup">
-<form action="https://mines.us7.list-manage.com/subscribe/post?u=810de7cc1c95c4d0926a32227&amp;id=27a0346aa4" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
-	<label for="mce-EMAIL">Subscribe to our monthly newsletter</label>
-	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_810de7cc1c95c4d0926a32227_27a0346aa4" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-    </div>
-</form>
-</div>
-
-<!--End mc_embed_signup-->
-							</div>
 							
 					</div>	
 				</div>
