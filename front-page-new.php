ba4b1039ffda5be61439a53adf77651f7ec08f02
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2019-03-13 14:01:12 -0600

    Update front-page-new.php

--- front-page-new.php
+++ front-page-new.php
@@ -1,8 +1,8 @@
 <?php 
-  /*Home Page New*/
+  /*Template Name: Home Page New*/
 	get_header();
 	get_template_part('inc/slider-home');
-  get_template_part('inc/intro-home');
+  	get_template_part('inc/intro-home');
 	get_template_part('inc/funnels-home');
 	get_template_part('inc/logo-slider');
 	get_footer(); 
