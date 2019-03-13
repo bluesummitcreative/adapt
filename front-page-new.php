0f996c9baf20e0b499279351b60963abc693e30c
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2019-03-13 13:36:08 -0600

    Create front-page-new.php

---
+++ front-page-new.php
@@ -0,0 +1,9 @@
+<?php 
+  /*Home Page New*/
+	get_header();
+	get_template_part('inc/slider-home');
+  get_template_part('inc/intro-home');
+	get_template_part('inc/funnels-home');
+	get_template_part('inc/logo-slider');
+	get_footer(); 
+?>
