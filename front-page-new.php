f82d972ec11f6f0ae904d268f285dedd688e09f2
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2019-03-14 11:58:06 -0600

    Update front-page-new.php

--- front-page-new.php
+++ front-page-new.php
@@ -68,7 +68,7 @@
 						if ( $query->have_posts() ) {
 							while ( $query->have_posts() ) {
 								$query->the_post(); ?>
-						<li><a href="https://adapt.mines.edu/news-and-events/"><?php echo get_the_excerpt(); ?></a></li>
+						<li><a href="https://adapt.mines.edu/membership/member-list"><?php echo get_the_excerpt(); ?></a></li>
 							<?php }
 						} else {
 							// no posts found
