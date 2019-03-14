85e63be45debe4781488124dd17cada1514c193b
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2019-03-14 10:01:51 -0600

    Update front-page-new.php

--- front-page-new.php
+++ front-page-new.php
@@ -42,7 +42,32 @@
 			</div>
 			<div class="col-sm-4">
 				<div class="funnel">
-					<?php the_field ('funnel5');?>				
+					<ul>
+					<?php
+						// WP_Query arguments
+						$args = array(
+							'post_type'              => array( '_adapt_in_the_news' ),
+							'post_status'            => array( 'publish' ),
+							'posts_per_page'         => '3',
+						);
+
+						// The Query
+						$query = new WP_Query( $args );
+
+						// The Loop
+						if ( $query->have_posts() ) {
+							while ( $query->have_posts() ) {
+								$query->the_post(); ?>
+						<li><a href="<?php the_permalink(); ?>"><?php the_excerpt(); ?></a></li>
+							<?php }
+						} else {
+							// no posts found
+						}
+
+						// Restore original Post Data
+						wp_reset_postdata();
+					?>
+					</ul>
 				</div>				
 			</div>
 		
